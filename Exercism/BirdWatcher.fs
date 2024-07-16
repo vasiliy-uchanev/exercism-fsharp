728eb297b364e22e043154292cabfaf03ff3d8f5
Vasiliy Uchanev <vasiliy.uchanev@servicetitan.com>
2024-07-16 12:48:12 -0700

    BirdWatcher

---
+++ Exercism/BirdWatcher.fs
@@ -0,0 +1,21 @@
+﻿module BirdWatcher
+
+let lastWeek: int[] = [| 0; 2; 5; 3; 7; 8; 4 |]
+
+let yesterday (counts: int[]) : int = counts[counts.Length - 2]
+
+let total (counts: int[]) : int = Array.sum counts
+
+let dayWithoutBirds (counts: int[]) : bool = Array.contains 0 counts
+
+let incrementTodaysCount (counts: int[]) : int[] =
+    let currentCount = counts[counts.Length - 1]
+    counts[counts.Length - 1] <- currentCount + 1
+    counts
+
+let unusualWeek (counts: int[]) : bool =
+    match counts with
+    | [| _; 0; _; 0; _; 0; _ |] -> true
+    | [| _; 10; _; 10; _; 10; _ |] -> true
+    | [| 5; _; 5; _; 5; _; 5 |] -> true
+    | _ -> false
--- Exercism/Program.fs
+++ Exercism/Program.fs
@@ -2,13 +2,29 @@
 open System
 
 printfn "awaiting input"
-let input = Console.ReadLine()
-let guess = int input
-printfn $"{GuessingGame.reply guess}"
-
-// let date  = BookingUpForBeauty.schedule "2024-07-01 12:00:00"
-// let dateString = date.ToString()
-// Console.WriteLine dateString
+// let input = Console.ReadLine()
+// let guess = int input
+// printfn $"{GuessingGame.reply guess}"
 //
-// let hasPassed = BookingUpForBeauty.hasPassed date
-// Console.WriteLine (hasPassed.ToString())
+// // let date  = BookingUpForBeauty.schedule "2024-07-01 12:00:00"
+// // let dateString = date.ToString()
+// // Console.WriteLine dateString
+// //
+// // let hasPassed = BookingUpForBeauty.hasPassed date
+// // Console.WriteLine (hasPassed.ToString())
+
+
+let lastWeek = BirdWatcher.lastWeek
+let yesterday = BirdWatcher.yesterday lastWeek
+
+printfn $"{yesterday}"
+
+let total = BirdWatcher.total lastWeek
+printfn $"{total}"
+
+let dayWithoutBirds = BirdWatcher.dayWithoutBirds lastWeek
+printfn $"{dayWithoutBirds}"
+
+let incremented = BirdWatcher.incrementTodaysCount lastWeek
+let strings = incremented |> Array.map (_.ToString()) |> Array.fold (fun x y -> x + ", " + y) ""
+printfn $"{strings}"
