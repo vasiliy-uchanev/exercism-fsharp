8a820e1292af6525c12e51ba04c69fa83cafdb83
Vasiliy Uchanev <vasiliy.uchanev@servicetitan.com>
2024-07-16 12:14:58 -0700

    Guessing game

---
+++ Exercism/GuessingGame.fs
@@ -0,0 +1,9 @@
+﻿module GuessingGame
+
+let reply (guess: int): string =
+    match guess with
+    | 42 -> "Correct"
+    | i when i = 41 || i = 43 -> "So close"
+    | i when i < 41 -> "Too low"
+    | _ -> "Too high"
+    
--- Exercism/Program.fs
+++ Exercism/Program.fs
@@ -1,14 +1,14 @@
 ﻿module Program
-
 open System
 
-// printfn "awaiting input"
-// let input = Console.ReadLine()
-// printfn $"{Bob.response input}"
+printfn "awaiting input"
+let input = Console.ReadLine()
+let guess = int input
+printfn $"{GuessingGame.reply guess}"
 
-let date  = BookingUpForBeauty.schedule "2024-07-01 12:00:00"
-let dateString = date.ToString()
-Console.WriteLine dateString
-
-let hasPassed = BookingUpForBeauty.hasPassed date
-Console.WriteLine (hasPassed.ToString())
+// let date  = BookingUpForBeauty.schedule "2024-07-01 12:00:00"
+// let dateString = date.ToString()
+// Console.WriteLine dateString
+//
+// let hasPassed = BookingUpForBeauty.hasPassed date
+// Console.WriteLine (hasPassed.ToString())
